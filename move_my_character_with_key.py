03a77a998b079e6bc5c9e77f82a3f455fd1bc885
Gihyeon(Hamzzi) <hamzzi99@naver.com>
2024-09-30 21:44:05 +0900

    이제 집도착
    하아.. 수업 너무많아...

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,8 +1,8 @@
 from pico2d import *
 open_canvas()
-character = load_image('MySprite.png')
+character = load_image('realsprite.png')
 frame = 0
-for x in range(0, 800, 10):
+for x in range(0, 800, 10): #원래 10
  clear_canvas()
  character.clip_draw(frame * 100, 0, 100, 100, x, 90)
  update_canvas()
